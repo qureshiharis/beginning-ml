8df6c897917e806169d7b902cf2acbe98e19b0f7
haris <haris.qureshi96@gmail.com>
2019-12-31 23:51:53 +0500

    slicing training dataset on svm

--- svm_example.py
+++ svm_example.py
@@ -3,6 +3,11 @@ from sklearn import svm
 # Creating classifier with linear decision boundary
 clf = svm.SVC(kernel='linear')
 
+# Training dataset down to 1% of its original size, tossing out 99% of the training data
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
+
+
 # Training classifier
 clf.fit(features_train, labels_train)
 
